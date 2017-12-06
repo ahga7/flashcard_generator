d6b63804a20bfab1c407489295e2f5695b4ca87b
ahmed <zamy_e@hotmail.com>
2017-12-06 00:33:14 -0600

    flashCards updated

---
+++ questions.js
@@ -0,0 +1,14 @@
+exports.questions = [
+	{
+		full: 'The rear window shape of a pre 1957 beetle is an oval.',
+		cloze: 'oval'
+	},
+	{
+		full: 'notchback, squareback, and fastback are all type 3.',
+		cloze: '3'
+	},
+	{
+		full: 'the last volkswagen type 1 "beetle" was produced in 2003.',
+		cloze: '2003'
+	}
+]
